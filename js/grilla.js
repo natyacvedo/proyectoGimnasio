80dc384b3d7bc2076973a94fb734c09eeea495c7
Robazan <rodrigo.kpo2800@gmail.com>
2021-12-13 15:53:35 -0300

    se agrega w-20 a la grill

--- js/grilla.js
+++ js/grilla.js
@@ -10,10 +10,9 @@ function crearColumnas(item) {
   let grilla = document.querySelector("#grilla");
   grilla.innerHTML += `<div class="col-12 col-md-4 col-lg-3 mb-3">
   <div class="card">
-    <img src="${item.url}" class="card-img-top" alt="${item.producto}" >
+    <img src="${item.url}" class="card-img-top w-20" alt="${item.producto}" >
     <div class="card-body">
       <h5 class="card-title">${item.descripcion}</h5>
-      <p class="card-text">ggggggg</p>
     </div>
   </div>
 </div>`;
